e01efa64910ffa71873b87029fbf163c203fa5af
Ingri109 <stalker.orest.109@gmail.com>
2024-05-10 15:27:04 +0200

    0.0.16-7

--- components/app_components/account.tsx
+++ components/app_components/account.tsx
@@ -5,7 +5,7 @@ import InfoUser from "./infoUser";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-import AddPartner from "./addPartner";
+import AddPartner from "./addpartner";
 import InfoPartner from "./infoPartner";
 import Link from "next/link";
 
