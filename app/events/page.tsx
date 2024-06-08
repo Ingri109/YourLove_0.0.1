e6ff529e894ab0b34cc83c2dd10c2c546835446e
Ingri109 <stalker.orest.109@gmail.com>
2024-06-08 20:05:04 +0200

    0.0.24-1

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -154,7 +154,7 @@ export default function Events() {
     return (
       <div className="absolute top-0 left-0 flex justify-center items-center w-full h-full">
         <div className="flex flex-col justify-center items-center w-11/12 max-h-full px-[2px] py-[12px] bg-color1_2 bg-opacity-10 backdrop-blur-md rounded-[10px] shadow-[0_15px_30px_7px_rgba(0,0,0,0.35)] xl:w-9/12 lg:w-10/12 lg:px-[2px] lg:py-[12px] md:w-11/12 md:px-[2px] md:py-[18px] sm:w-4/6 animate-scaleIn">
-          <h1 className="text-[24px] text-white font-bold text-center text-balance w-full md:text-[32px]">У вас ще немає партнера дабавте його і тільки тоід зможете зайти на цю сторінку</h1>
+          <h1 className="text-[24px] text-white font-bold text-center text-balance w-full md:text-[32px]">У вас ще немає партнера дабавте його і тільки тоді зможете зайти на цю сторінку</h1>
           <Link href={'/account'} className=" bg-color4_1 text-white text-[12px] font-semibold mt-[10px] px-[28px] py-1.5 rounded-md hover:bg-color4 focus:bg-color4_3 md:mt-[28px] md:text-[18px] md:rounded-xl">Перейти на сторінку Акаунта</Link>
         </div>
       </div>
--- middleware.ts
+++ middleware.ts
@@ -3,20 +3,20 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
+  let data;
 
   const supabase = createMiddlewareClient({ req, res });
   const {
     data: { session },
   } = await supabase.auth.getSession();
 
-  const { data, error: userError } = await supabase
-    .from("users_info")
-    .select("name")
-    .eq("id", session?.user?.id)
-    .single();
-
+  if (session?.user?.id) {
+    const { data: userData, error: userError } = await supabase.from("users_info").select("name").eq("id", session?.user?.id).single();
+    data = userData;
+  }
   let publivUrls = ["/", "/auth/callback"];
 
+  console.log(data);
   if (session?.user?.id) {
     if (!data) {
       publivUrls.push("/registerForm");
@@ -35,24 +35,24 @@ export async function middleware(req: NextRequest) {
     publivUrls.push("/login/reset");
   }
 
-  // if (
-  //   publivUrls.includes(req.nextUrl.pathname) ||
-  //   (session?.user?.id && req.nextUrl.pathname === "/")
-  // ) {
-  //   return res;
-  // }
+  if (
+    publivUrls.includes(req.nextUrl.pathname) ||
+    (session?.user?.id && req.nextUrl.pathname === "/")
+  ) {
+    return res;
+  }
 
-  // if (!session?.user?.id) {
-  //   return NextResponse.redirect(new URL("/login", req.url));
-  // }
-  // if (!data) {
-  //   return NextResponse.redirect(new URL("/registerForm", req.url));
-  // } 
+  if (!session?.user?.id) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+  if (!data) {
+    return NextResponse.redirect(new URL("/registerForm", req.url));
+  }
 
-  // if (session?.user?.id ) {
-  //   return NextResponse.redirect(new URL("/account", req.url));
-  // }
-  
+  if (session?.user?.id ) {
+    return NextResponse.redirect(new URL("/account", req.url));
+  }
+
   return res;
 }
 
