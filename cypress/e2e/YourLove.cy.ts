c99e1e87cb0d77b39f5aec1cbb74dd038a33fd25
Ingri109 <stalker.orest.109@gmail.com>
2024-06-11 18:43:10 +0200

    test_1

--- cypress/e2e/YourLove.cy.ts
+++ cypress/e2e/YourLove.cy.ts
@@ -1,5 +1,5 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('https://your-love-0-0-1-fzi8v81rv-ingri109s-projects.vercel.app/')
   })
 })
